1d012370f591d64b93982fbb5102a1c6bbc84153
lwrb172 <lwrbielak@gmail.com>
2024-05-10 11:00:05 +0200

    lab7z1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import org.example.database.DatabaseConnection;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+        String createSQLTable = "CREATE TABLE IF NOT EXISTS accounts( " +
+                "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                "username TEXT NOT NULL," +
+                "password TEXT NOT NULL)";
+
+        DatabaseConnection dbconnection = new DatabaseConnection();
+        dbconnection.connect("database.db");
+        PreparedStatement preparedStatement = dbconnection.getConnection().prepareStatement(createSQLTable);
+        preparedStatement.executeUpdate();
+        dbconnection.disconnect();
     }
 }
--- src/main/java/org/example/database/DatabaseConnection.java
+++ src/main/java/org/example/database/DatabaseConnection.java
@@ -1,4 +1,35 @@
 package org.example.database;
 
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+    private Connection connection;
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void connect(String path)  {
+        try {
+            connection = DriverManager.getConnection(String.format("jdbc:sqlite:%s", path));
+            System.out.println("Connected");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void disconnect() {
+        try {
+            if(connection != null && connection.isClosed() ) {
+                connection.close();
+            }
+            System.out.println("Disconnected");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
